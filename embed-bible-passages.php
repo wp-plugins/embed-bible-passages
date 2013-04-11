a13647efc23467b29c6eb78dc2b0093d9c1abc7d
drmikegreen <drmikegreen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-11 15:02:26 +0000

    This version implements: 1. IMPORTANT CHANGE: A correction to the shortcode naming system: The shortcode is now [embed_bible_passage reading_plan='bcp']. (reading-plan has been changed to reading_plan. See http://codex.wordpress.org/Shortcode_API.) 2. The ability to use other than the default reading plan. 3. An optional ability for users to select passages for days other than the current day by clicking on a calendar. 4. An mp3 audio file is used for iPhone and iPad, rather than the default Flash audio file.
    git-svn-id: https://plugins.svn.wordpress.org/embed-bible-passages/trunk@696018 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- embed-bible-passages.php
+++ embed-bible-passages.php
@@ -7,18 +7,16 @@
 /*
 Plugin Name: Embed Bible Passages
 Plugin URI:
-Description: Provides the ability to embed Bible Readings from the ESV Bible Web Service (http://www.esvapi.org/api#readingPlanQuery) into a post or page using shortcode of the form [embed_bible_passage reading-plan='bcp'].
+Description: Provides the ability to embed Bible Readings from the ESV Bible Web Service (http://www.esvapi.org/api#readingPlanQuery) into a post or page using shortcode of the form [embed_bible_passage reading_plan='bcp'].
 Version: 0.7
 Author: M.D. Green
 Author URI: http://saesolved.com/
 
 Copyright 2011-2013 M.D. Green, SaeSolved:: LLC
 
-This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the
-Free Software Foundation; either version 2 of the License, or (at your option) any later version.
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
 */
 
