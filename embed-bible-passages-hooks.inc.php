e4b6db77564c32d8f8ed42b7f4591369b621f629
drmikegreen <drmikegreen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-18 14:39:18 +0000

    Commit Version 1.1, which changes loading readings from jQuery.load() to the WordPress AJAX system.
    git-svn-id: https://plugins.svn.wordpress.org/embed-bible-passages/tags/1.1@699803 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- embed-bible-passages-hooks.inc.php
+++ embed-bible-passages-hooks.inc.php
@@ -3,9 +3,14 @@
 if (class_exists('EmbedBiblePassages')) {
 	$bible_passage = new EmbedBiblePassages();
 	if (isset($bible_passage)) {
-		add_action('wp_head', array(&$bible_passage, 'addCSS'), 1);
-		add_action('wp_enqueue_scripts', array(&$bible_passage, 'enqueueScripts'));
-		add_action('wp_footer', array(&$bible_passage, 'addDatePicker'), 1);
+		if (is_admin()) { // http://codex.wordpress.org/AJAX_in_Plugins#Ajax_on_the_Viewer-Facing_Side
+			add_action('wp_ajax_put_bible_passage', array(&$bible_passage, 'putBiblePassage'));
+			add_action('wp_ajax_nopriv_put_bible_passage', array(&$bible_passage, 'putBiblePassage'));
+		} else {
+			add_action('wp_head', array(&$bible_passage, 'addCSS'), 1);
+			add_action('wp_enqueue_scripts', array(&$bible_passage, 'enqueueScripts'));
+			add_action('wp_footer', array(&$bible_passage, 'addDatePicker'), 1);
+		}
 	}
 }
 
